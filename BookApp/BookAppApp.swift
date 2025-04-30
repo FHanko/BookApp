fd5b5f7f1ab100ae6ae12bab875b825b385df622
user276637 <user276637@FF009.local>
2025-04-30 22:25:48 +0200

    Init

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by user276637 on 4/30/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
